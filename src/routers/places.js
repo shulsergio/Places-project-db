d3eb2964caadd8888703a2da3934d9b933c01cdd
Sergey Shulga <shul.sergio@gmail.com>
2024-12-03 23:22:35 +0200

    v 1.34 fix err register/login/out user

--- src/routers/places.js
+++ src/routers/places.js
@@ -11,14 +11,14 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const placesRouter = Router();
 
-placesRouter.get('/places', ctrlWrapper(getAllPlacesController));
-placesRouter.get('/places/:placeId', ctrlWrapper(getPlaceByIdController));
+placesRouter.get('/', ctrlWrapper(getAllPlacesController));
+placesRouter.get('/:placeId', ctrlWrapper(getPlaceByIdController));
 
-placesRouter.post('/places', ctrlWrapper(createPlaceController));
+placesRouter.post('/', ctrlWrapper(createPlaceController));
 
-placesRouter.delete('/places/:placeId', ctrlWrapper(deletePlaceController));
+placesRouter.delete('/:placeId', ctrlWrapper(deletePlaceController));
 
-placesRouter.put('/places/:placeId', ctrlWrapper(upsertPlaceController));
-placesRouter.patch('/places/:placeId', ctrlWrapper(patchPlaceController));
+placesRouter.put('/:placeId', ctrlWrapper(upsertPlaceController));
+placesRouter.patch('/:placeId', ctrlWrapper(patchPlaceController));
 
 export default placesRouter;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import placesRouter from './routers/places.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import router from './routers/index.js';
 
 export const setupServer = () => {
   const app = express();
   const PORT = process.env.PORT || 4000;
-
+  app.use(express.json());
   app.use(cors());
   app.use(
     pino({
@@ -20,9 +20,10 @@ export const setupServer = () => {
   app.get('/', (req, res) => {
     res.json({ message: 'all is ok' });
   });
-  app.use(errorHandler);
-  app.use(placesRouter);
+
+  app.use(router);
   app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} port`);
